55b63416139b01c055b83dc9adbb1549fb2c8be5
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-28 04:40:13 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.common.by import By
 
 # Chrome options (GitHub Actions mühitinə uyğunlaşdırmaq)
 chrome_options = Options()
