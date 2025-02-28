e16fed17ed6a25275457b7d3a52c44b3773e3cd6
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-28 05:47:40 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import time
+from webdriver_manager.chrome import ChromeDriverManager
 
 def update_token():
-    # Chrome'u başlatmaq üçün konfiqurasiya
-    options = Options()
-    options.add_argument("--headless")  # Headless rejimində çalışır (ekran olmadan)
-    driver = webdriver.Chrome(options=options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    
+    # Daha sonra selenium kodunuzun qalan hissəsi...
+
     
     # URL-ni yükləyirik
     url = "https://www.ecanlitvizle.app/xezer-tv-canli-izle/"
