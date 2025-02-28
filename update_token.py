2bf8e6f3c4d4ba2c6a709a8a0ad9a34eb61b06f2
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-28 04:38:59 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-# WebDriver ayarları
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Başlıq olmadan işləmək
-driver = webdriver.Chrome(options=options)
+# Chrome options (GitHub Actions mühitinə uyğunlaşdırmaq)
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # GUI olmadan işlətmək
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/chromium-browser"  # GitHub Actions üçün uyğun Chrome
+
+# WebDriver ilə browser açılır
+driver = webdriver.Chrome(options=chrome_options)
 
 # Tokenin olduğu səhifə URL-si
 url = "SƏHİFƏ_URL"  # Tokeni tapacağın səhifənin URL-si
