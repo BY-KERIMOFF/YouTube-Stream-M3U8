dae3b8656175becdf58630683c0aa884a4765dbb
MONSTER__065 <tebrizkerimli8794@gmail.com>
2025-02-28 05:05:49 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def update_token():
     # M3U8 linki alın
-    url = "https://live.cdn-canlitv.com/aztv2.m3u8?anahtar=YOUR_TOKEN_HERE"
+    url = "https://www.ecanlitvizle.app/xezer-tv-canli-izle/?anahtar=YOUR_TOKEN_HERE"
     
     # Selenium ilə səhifəni açmaq
     chrome_options = Options()
