5c2da54594a7111661870b0404803baa83b0f6af
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-01 09:56:46 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -83,7 +83,7 @@ def update_github_repo(github_token, m3u8_link, channel_name):
 def main():
     # Yeni tokeni daxil et
     new_token = "NrfHQG16Bk4Qp4yo0YWCaQ"  # Yenilənməli olan token
-    github_token = "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"  # Burada öz GitHub tokenini yaz
+    github_token = "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"  # Burada öz GitHub tokenini yaz
 
     # Verilən tokenli linklər
     tokenli_linkler = {
