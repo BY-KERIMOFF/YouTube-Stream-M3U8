d42d2746b3a987d164082d671d2f1fd1872d3887
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-02 01:33:04 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -1,3 +1,4 @@
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -10,8 +11,12 @@ import json
 
 def get_m3u8_from_network():
     try:
+        # Unikal bir qovluq yaradın
+        user_data_dir = tempfile.mkdtemp()
+
         options = Options()
-        # options.add_argument("--headless")  # Headless rejimini sınaqdan keçirin
+        options.add_argument(f"--user-data-dir={user_data_dir}")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
