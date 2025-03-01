f5ee4655c2cb3ac783771bc0ce9fd44efc0f4887
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-02 01:18:21 +0400

    Update update_token.py

--- update_token.py
+++ update_token.py
@@ -1,27 +1,23 @@
-import os
-import time
-import json
-import re
-import requests
-import base64
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from urllib.parse import urlparse, parse_qs, urlencode
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 def get_m3u8_from_network():
     try:
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")  # Headless rejimini sınaqdan keçirin
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+
         url = "https://www.ecanlitvizle.app/xezer-tv-canli-izle/"
         driver.get(url)
 
@@ -53,44 +49,13 @@ def get_m3u8_from_network():
         print(f"Xəta: {e}")
         return None
 
-def update_token_in_url(url, new_token):
-    if not url:
-        return None
-
-    parsed_url = urlparse(url)
-    query_params = parse_qs(parsed_url.query)
-
-    query_params["tkn"] = [new_token]  # Yeni tokeni əlavə et
-
-    updated_query = urlencode(query_params, doseq=True)
-    updated_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}?{updated_query}"
-    return updated_url
-
-def write_to_local_file(m3u8_link):
-    if not m3u8_link:
-        return "M3U8 linki tapılmadı, fayl yenilənmədi."
-
-    try:
-        new_content = f"#EXTM3U\n#EXTINF:-1,xezer tv\n{m3u8_link}\n"
-        file_path = os.path.abspath("token.txt")
-
-        with open(file_path, "w") as file:
-            file.write(new_content)
-
-        return "Yerli fayl uğurla yeniləndi."
-    except Exception as e:
-        return f"Fayla yazma xətası: {e}"
-
 def main():
-    new_token = os.getenv("NEW_TOKEN", "default_token")
-
     m3u8_link = get_m3u8_from_network()
     if not m3u8_link:
         print("M3U8 tapılmadı.")
         return
 
-    updated_m3u8_link = update_token_in_url(m3u8_link, new_token)
-    print(write_to_local_file(updated_m3u8_link))
+    print(f"M3U8 linki: {m3u8_link}")
 
 if __name__ == "__main__":
     main()
