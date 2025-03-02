ea79248139c50bb526e08a3bd17edc75bbdf603e
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 02:04:14 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,34 +1,16 @@
 import requests
 import re
-from datetime import datetime
 
-# Tokenli link
+# URL-dəki tokeni çəkmək
 url = "https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn=Fh2F2HhcbuZaxDX8hYPQqQ&tms=1740960002"
+response = requests.get(url)
 
-# Linkdəki tokeni tapmaq üçün regex istifadə edirik
-def get_new_token(url):
-    try:
-        response = requests.get(url)
-        # URL-dəki tokeni regex ilə çəkirik
-        token_match = re.search(r'tkn=([A-Za-z0-9]+)', response.url)
-        
-        if token_match:
-            return token_match.group(1)  # Yeni tokeni qaytarır
-        else:
-            print("Token tapılmadı.")
-            return None
-    except Exception as e:
-        print(f"Xəta baş verdi: {e}")
-        return None
-
-# Yeni tokeni al
-new_token = get_new_token(url)
-
+# Tokeni tapmaq (əgər URL-dəki token dəyişirsə)
+new_token = re.search(r'tkn=([^&]+)', response.url)
 if new_token:
-    # Yeni tokeni faylda saxlamaq
+    new_token = new_token.group(1)
+    # Tokeni fayla yazmaq
     with open("new_token.txt", "w") as file:
-        file.write(f"Yeni Token: {new_token}\n")
-        file.write(f"Tarix: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-    print(f"Yeni token {new_token} fayla yazıldı.")
+        file.write(new_token)
 else:
-    print("Yeni token tapılmadı.")
+    print("Token tapılmadı")
