13e2fe09fb4dbbc5db8a68e249831389dfeecf18
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 01:28:08 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 # Sayta sorğu göndəririk
 url = 'https://www.ecanlitvizle.app'
@@ -12,8 +13,11 @@ soup = BeautifulSoup(html, 'html.parser')
 # Kanal linklərini tapırıq (XPath ilə deyil, CSS selector ilə)
 channels = soup.find_all('a', class_='channel-link')  # Bu sinifi təhlil etməli olacaqsınız
 
+# Faylın tam yolunu göstəririk
+output_path = os.path.join(os.getcwd(), 'channels.m3u')
+
 # M3U faylını yazırıq
-with open('channels.m3u', 'w') as file:
+with open(output_path, 'w') as file:
     file.write('#EXTM3U\n')  # M3U başlığı
     for channel in channels:
         name = channel.text.strip()
@@ -21,4 +25,4 @@ with open('channels.m3u', 'w') as file:
         file.write(f'#EXTINF:-1,{name}\n')
         file.write(f'{url}\n')
 
-print('M3U faylı yaradıldı.')
+print(f'M3U faylı yaradıldı: {output_path}')
