989d2d5581fdf206c561504f0f4a1f27d73ab690
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-02 07:59:02 +0400

    Update script.py

--- script.py
+++ script.py
@@ -11,8 +11,9 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-extensions")  # Uzantıları bağlamaq
 
-# Webdriver başlat
-browser = webdriver.Chrome(options=options)
+# ChromeDriver yolunu əlinizlə təyin edin
+chrome_driver_path = "/path/to/chromedriver"  # Burada öz yolunuzu yazın
+browser = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
 # Saytı aç
 url = "https://www.ecanlitvizle.app/xezer-tv-canli-izle/"
