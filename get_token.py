01cfd5fee170d2a3d10f2104732bdb1d4d16574b
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 04:13:52 +0400

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,33 +1,20 @@
+import re
 import requests
 
-# AJAX sorğusunu göndərmək
-ajax_url = "https://www.ecanlitvizle.app/xezer-tv-canli-izle/"  # Tokeni əldə etməli olduğumuz URL
+# URL daxil edin
+url = "https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn=JCm4CeptUoCkaoqwLPifNQ&tms=1740967951"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-}
+# URL-dən tokeni çıxarmaq
+token_match = re.search(r'tkn=([A-Za-z0-9-_]+)', url)
+if token_match:
+    token = token_match.group(1)
+    print(f"Token tapıldı: {token}")
+else:
+    print("Token tapılmadı!")
 
-try:
-    # AJAX sorğusunu göndəririk
-    response = requests.get(ajax_url, headers=headers)
-    if response.status_code == 200:
-        print("AJAX sorğusu uğurla alındı.")
-        print("Cavab məzmunu:")
-        print(response.text)  # Tam məzmunu çap edirik
-
-        # JSON cavabına çevirməyi sınamaq
-        try:
-            json_response = response.json()
-            print(json_response)
-            if "token" in json_response:
-                token = json_response["token"]
-                print(f"Yeni token tapıldı: {token}")
-            else:
-                print("Token JSON içində tapılmadı.")
-        except ValueError as e:
-            print(f"Cavab JSON formatında deyil: {e}")
-
-    else:
-        print(f"AJAX sorğusu uğursuz oldu. Status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print(f"Şəbəkə problemi oldu: {e}")
+# Tokeni istifadə edərək sorğu göndərmək
+response = requests.get(url)
+if response.status_code == 200:
+    print("Sorğu uğurla yerinə yetirildi!")
+else:
+    print(f"Error: {response.status_code}")
