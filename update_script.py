518ee1d1e6ff97c12ce86c023694466a8b0fb5ad
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 07:48:30 +0400

    Update update_script.py

--- update_script.py
+++ update_script.py
@@ -1,36 +1,25 @@
 import requests
-from bs4 import BeautifulSoup
 
 # URL tərtib et
-url = 'https://www.ecanlitvizle.app/xezer-tv-canli-izle/'
+url = 'https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn=XdBMwsPAdMN_l8EqNmVx5A&tms=1740980749'
 
-# Saytı çək
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+# Sorğu başlıqları
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
+    'Referer': 'https://www.ecanlitvizle.app/',
+    'Origin': 'https://www.ecanlitvizle.app'
+}
 
-# Linki tapmaq
-script_tag = soup.find('script', string=lambda t: t and 'tkn=' in t)  # 'text' əvəzinə 'string' istifadə edin
+# GET sorğusu göndər
+response = requests.get(url, headers=headers)
 
-if script_tag:
-    # Tokeni çıxar
-    script_content = script_tag.string
-    token_start = script_content.find('tkn=') + 4  # 'tkn=' ifadəsindən sonra başlayır
-    token_end = script_content.find('&', token_start)  # '&' simvoluna qədər davam edir
-    token = script_content[token_start:token_end]  # Tokeni çıxar
-
-    with open('token.txt', 'w') as file:
-        file.write(token)
-    print("✅ Token tapıldı.")
-
-    # Yeni link yarat
-    new_link = f"https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn={token}&tms=1740980455"
-    with open("stream.m3u8", "w") as file:
-        file.write(new_link)
-    print("✅ stream.m3u8 yeniləndi.")
-else:
-    print("❌ Token tapılmadı.")
+# Cavabı yoxla
+if response.status_code == 200:
+    print("✅ Sorğu uğurlu oldu.")
     
-    # Əgər token tapılmasa, sabit link əlavə et
+    # Cavab məzmununu fayla yaz
     with open("stream.m3u8", "w") as file:
-        file.write("https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn=w3nxqj5RKo4JLfvPLv9oxA&tms=1740980455")
-    print("✅ stream.m3u8 sabit link ilə yeniləndi.")
+        file.write(response.text)
+    print("✅ stream.m3u8 faylı yeniləndi.")
+else:
+    print(f"❌ Sorğu uğursuz oldu. Status kodu: {response.status_code}")
