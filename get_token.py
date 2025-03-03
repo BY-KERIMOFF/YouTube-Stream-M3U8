8d0410574d93b16b67a5f25e32bcc13fe9632cfb
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 04:51:19 +0400

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,13 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_token():
     # Set up the WebDriver
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--headless')  # Başsız rejimdə işləyir
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
