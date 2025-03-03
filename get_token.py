1ff7dca5870538188bc1a29994050012cda9d960
Bykerimoff <tebrizkerimli8794@gmail.com>
2025-03-03 05:36:09 +0400

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -25,6 +26,7 @@ time.sleep(5)
 
 # Tokeni tapmağa çalışırıq
 token_element = None
+token = None
 try:
     token_element = driver.find_element("xpath", "//script[contains(text(),'tkn=')]")  # Yeni metod istifadə edilir
     token_script = token_element.get_attribute('innerHTML')
@@ -39,6 +41,22 @@ try:
     print("✅ Token fayla yazıldı.")
 except Exception as e:
     print(f"❌ Token tapılmadı! Error: {e}")
+
+# Əgər token tapılmadısa, köhnə stream.m3u8 faylını sil və yeni link əlavə et
+if not token:
+    if os.path.exists("stream.m3u8"):
+        os.remove("stream.m3u8")  # Köhnə faylı silirik
+    # Yeni linki əlavə edirik
+    new_link = "https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn=new_token&tms=1740969806"
+    with open("stream.m3u8", "w") as f:
+        f.write(new_link)
+    print("✅ Köhnə token tapılmadı. Yeni tokenlə stream.m3u8 faylı yeniləndi.")
+else:
+    # Token tapılarsa stream.m3u8 faylını yeniləyirik
+    new_m3u8_link = f"https://ecanlitv3.etvserver.com/xazartv.m3u8?tkn={token}&tms=1740969806"
+    with open("stream.m3u8", "w") as f:
+        f.write(new_m3u8_link)
+    print("✅ stream.m3u8 faylı yeniləndi.")
 
 # Driver-i bağlayırıq
 driver.quit()
