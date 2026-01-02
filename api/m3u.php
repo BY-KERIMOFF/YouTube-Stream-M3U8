05960977eeffb19d3992d5740a662f0ae0043f71
LOST-LOVERS <tebrizkerimli8794@gmail.com>
2026-01-02 21:28:46 +0300

    Update m3u.php

--- api/m3u.php
+++ api/m3u.php
@@ -7,8 +7,8 @@ $data = json_decode(file_get_contents($json_url), true);
 echo "#EXTM3U\n";
 
 foreach ($data as $ch) {
-    $name = $ch['name'];
-    $url = $ch['m3u8'];
-    echo "#EXTINF:-1,{$name}\n";
-    echo $url . "\n";
+    if (!empty($ch['m3u8'])) {
+        echo "#EXTINF:-1,{$ch['name']} - Canlı Yayın\n";
+        echo $ch['m3u8'] . "\n";
+    }
 }
